a83cd0b0426ec2ae0d6c0e5f9e071bec0d8d04e2
joshua inocencio <snotbot1234@gmai.com>
2014-12-03 13:09:05 -0800

    yup

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,8 +3,28 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
-                me.game.world.addChild(titleImage, 1);
+                me.game.world.addChild( new me.Sprite(0, 0, me.loader.getImage("title-screen")), 3);
+                me.input.bindKey(me.input.KEY.ENTER, "start");
+                
+                me.game.world.addChild(new(me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this.font = new me.font("Arial", 46, "white");
+                        
+                    },
+                    
+                    draw: function(renderer){
+                       this.font.draw(renderer.getContext(), "Marioish", 450, 130);
+                       this.font.draw(renderer.getContext(), "press ENTER to play!", 250, 530);
+                    }
+                })));
+                
+              
+               this.handler = me.event.subscribe(me.event.KEYDOWN, function(action, keyCode, edge){
+                   if(action === "start"){
+                       me.state.change(me.state.PLAY);
+                   }
+               });
 	},
 	
 	
@@ -12,6 +32,6 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+            me.input.unbindKey(me.input.KEY.ENTER);
 	}
 });
