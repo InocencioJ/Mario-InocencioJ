e330e810a080f4c1a6c3af2140ac18c398f0b0d8
joshua inocencio <snotbot1234@gmai.com>
2014-12-09 14:02:23 -0800

    eating food

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -13,11 +13,14 @@ game.PlayerEntity = me.Entity.extend({
             }]);
 
         this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("bigIdle", [19]);
         this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+        this.renderable.addAnimation("bigWalk", [14, 15, 16, 17, 18, 19], 80);
         /*this code allows the player animation to move using to frames from player 8-13
          *the 80 at the end tell the animation how fast the animation is going to move*/
         this.renderable.setCurrentAnimation("idle");
         
+        this.big = false;
         this.body.setVelocity(5, 20);
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
         /*the velocity code sets the code so move at a specific speed
@@ -45,18 +48,29 @@ game.PlayerEntity = me.Entity.extend({
 
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
-
+        
+        if(!this.big){
         if (this.body.vel.x !== 0) {
             if (!this.renderable.isCurrentAnimation("smallWalk")) {
                 this.renderable.setCurrentAnimation("smallWalk");
+                this.renderable.setAnimationFrame();
                 /*this code makes the person who does the action smallwalk*/
             }
-        }
-        
-        else {
+        }else{
             this.renderable.setCurrentAnimation("idle");
-        }
-
+        } 
+    }else{
+      if (this.body.vel.x !== 0) {
+            if (!this.renderable.isCurrentAnimation("bigWalk")) {
+                this.renderable.setCurrentAnimation("bigWalk");
+                this.renderable.setAnimationFrame();
+                /*this code makes the person who does the action smallwalk*/
+            }
+        }else{
+            this.renderable.setCurrentAnimation("bigidle");
+        }   
+    }
+        
         this._super(me.Entity, "update", [delta]);
         return true;
         
@@ -69,13 +83,15 @@ game.PlayerEntity = me.Entity.extend({
             if(ydif <= -115){
                 response.b.alive = false;
             }else{
-                
-            
-            
             me.state.change(me.state.MENU);
         }
-        }
-    }
+      }else if(response.b.type ==='mushroom'){
+          this.big = true;
+          me.game.world.removeChild(response.b);
+      }
+   
+    
+   }
 
 
 });
